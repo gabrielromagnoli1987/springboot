ddd35719e7700ab33dab8f657ec35179b81bbcd8
gabrielromagnoli1987 <gabrielromagnoli1987@github.com>
2016-08-17 22:01:24 -0300

    Se agrega configuracion para cors

--- src/main/java/backend/Application.java
+++ src/main/java/backend/Application.java
@@ -1,11 +1,13 @@
 package backend;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -13,15 +15,24 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Application {
 
     public static void main(String[] args) {
+    	
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+    }
+    
+    
+    /* CORS: https://spring.io/guides/gs/rest-service-cors/#initial
+    También se puede configurar con annotations a nivel método o clase en los Controllers
+    @CrossOrigin(origins = "http://localhost:8000") */
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override	
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/cars").allowedOrigins("http://localhost:8000");
+            }
+        };
     }
 
 }
